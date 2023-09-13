f5e02c61326c057efe207eeb42f16e77a4bf32a6
Rifat0001 <mahmudrifat764@gmail.com>
2023-09-14 03:55:16 +0600

    delete

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const req = require('express/lib/request');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsztvpo.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -44,6 +44,14 @@ async function run() {
             }
             const query = { email: email };
             const result = await cartsCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        // for delete items 
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartsCollection.deleteOne(query);
             res.send(result)
         })
 
